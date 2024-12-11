ce692c90a256209a9861ffd4c64f8825f4b57afe
Aboubacar SANGARE <abouba.sang@outlook.com>
2024-12-11 16:23:31 +0000

    backend setup

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Middleware
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+// Here use the app.use to use the routes
+
+export default app;
