6fb07d524a0f3ed116496f981e18c046002ce713
sounkalo-mitic <sounkalo.sidibe@miticsarl.com>
2025-03-13 16:27:50 +0100

    Ajout des routes pour la réinitialisation du mot de passe et l'envoi d'un email de réinitialisation

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -9,6 +9,8 @@ import {
   logout,
   refreshToken,
   register,
+  resetPassword,
+  sendResetPasswordEmail,
   toggleUserStatus,
   updateUser,
   verifyOTP,
@@ -46,6 +48,9 @@ router.patch("/users/:id/toggle-status", toggleUserStatus);
 router.get("/users/id/:id", getUserById);   // Route pour obtenir un utilisateur par ID
 
 router.get("/users/role/:role", getUsersByRole); // Route pour obtenir les utilisateurs par rôle
+
+router.post("/send-reset-email", sendResetPasswordEmail);
+router.post("/reset-password", resetPassword);
 
 
 export default router;
